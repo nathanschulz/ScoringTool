6df09600efb85973ade274bb25e627a93eed0ff2
nathanschulz <nschulz4537@gmail.com>
2023-04-24 08:14:02 -0500

    changes

--- force-app/main/default/lwc/segmentCriteriaList/segmentCriteriaList.js
+++ force-app/main/default/lwc/segmentCriteriaList/segmentCriteriaList.js
@@ -1,3 +1,31 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 
-export default class SegmentCriteriaList extends LightningElement {}
+export default class SegmentCriteriaList extends LightningElement {
+	@track segmentCriteria = [{key: "a"},{key: "b"},{key: "c"},{key: "d"}];
+	@api startingPosition;
+
+	drop(e) {
+		console.log('dropping begins');
+		e.stopPropagation();
+		const dragVal = this.startingPosition;
+		const dropVal = e.target.title;
+		console.log('start index: ' + dragVal);
+		console.log('stop index: ' + dropVal);
+		if (dragVal === dropVal) {
+			console.log('position unchanged');
+			return false;
+		}
+		this.segmentCriteria.splice(dropVal, 0, this.segmentCriteria.splice(dragVal, 1)[0]);
+	}
+
+	dragStart(e) {
+		console.log('drag started!');
+		this.startingPosition = e.target.title;
+	}
+
+	dragOver(e) {
+		e.preventDefault();
+		return false;
+	}
+
+}
--- force-app/main/default/lwc/segmentCriterion/segmentCriterion.js
+++ force-app/main/default/lwc/segmentCriterion/segmentCriterion.js
@@ -1,3 +1,16 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class SegmentCriterion extends LightningElement {}
+export default class SegmentCriterion extends LightningElement {
+	@api criterion;
+	@api activeSections = [];
+
+	toggleSection(e) {
+        let currentsection = this.template.querySelector('.slds-section');
+        if (currentsection.className.search('slds-is-open') == -1) {
+            currentsection.className = 'slds-section slds-is-open';
+        } else {
+            currentsection.className = 'slds-section slds-is-close';
+        }
+	}
+
+}
